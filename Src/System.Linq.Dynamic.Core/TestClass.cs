67f8a5c1483a1a64c17f50658a0bc798a34c87bc
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-04-07 21:51:08 +0200

    Rename to System.Linq.Dynamic.Core

---
+++ Src/System.Linq.Dynamic.Core/TestClass.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Reflection;
+//using System.Reflection.Emit;
+
+//namespace System.Linq.Dynamic
+//{
+//    public class TestClass
+//    {
+//        public void X()
+//        {
+//            Type type = typeof (string);
+//            object[] arguments = { "", null };
+//        }
+//    }
+//}
